f035dfcea3ad77ad1e6ee1bb8eed3cb61f5f5602
ExpandingDev <tylersengia@gmail.com>
2022-01-09 19:50:57 -0500

    Fixing syntax errors

--- variance-units/app.py
+++ variance-units/app.py
@@ -1,11 +1,24 @@
 import logging
 import pathlib
 import sqlalchemy
+from sqlalchemy.orm import Session
 import connexion
 
-from config import UnitsConfig
-
 app = connexion.FlaskApp(__name__, specification_dir="openapi/")
+
+# Expose the bare FlaskApp for use by our WSGI
+application = app.app
+
+
+SQLALCHEMY_ECHO = True
+DATABASE_URI = "sqlite+pysqlite:///:memory:"
+
+# Initialize the database connection
+db = sqlalchemy.create_engine(DATABASE_URI, echo=SQLALCHEMY_ECHO)
+session = Session(db)
+
+# Load in our model
+from models import UnitModel
 
 if __name__ == "__main__":
     app.add_api("units-service.yaml")
